5be1c6002a86cf2132dce0efb98649cb6e66f1c2
Jonathan Loos <jonathan.loos@web.de>
2019-09-10 13:57:48 +0200

    Add the possibility of an alive check to Subscriber and Publisher

--- messaging/zmq_classes.py
+++ messaging/zmq_classes.py
@@ -1,33 +1,94 @@
+import time
+from threading import Thread
+
 import msgpack
 import zmq
+from netifaces import interfaces, ifaddresses, AF_INET
+
+ALIVE_TOPIC = "_alive"
 
 
 class Subscriber:
-    def __init__(self, ip, sub_port, subjects):
+    def __init__(self, ip, port, subjects):
         """
         Creates a Subscriber wrapping a zmq.SUB socket listening to the given subjects
         Args:
             ip(str)): the ip to subscribe to
-            sub_port(int): the port to subscribe on
+            port(int): the port to subscribe on
             subjects(list(str)): the subject to subscribe on
         """
-        self.ip = ip
-        self.sub_port = sub_port
+        self.ip_and_ports = {(ip, port): 0.0}  # the ip-port-combinations with their last alive time
         self.subjects = subjects
         # noinspection PyUnresolvedReferences
         self.subscriber = zmq.Context().socket(zmq.SUB)
+        self.check_alive_thread = None
+        self.timeout = 0
+        self.check_alive_subscriber = None
+
+    def add_additional_ips(self, ip_and_ports):
+        """
+        Add additional ip-port-combinations on which the subscriber will listen to data
+        Args:
+            ip_and_ports(list(tuple(str, int))): the list of additional ips and ports
+        Returns: (Subscriber) self
+        """
+        for ip, port in ip_and_ports:
+            self.ip_and_ports[(ip, port)] = 0.0
+        return self
+
+    def set_connection_timeout(self, timeout):
+        """
+        Sets a connection timeout after which all connections which did not recently send an ALIVE_TOPIC message
+        are disconnected
+        Args:
+            timeout(int): the timeout in milliseconds
+        """
+        self.check_alive_subscriber = Subscriber("", 0, []).add_additional_ips(self.ip_and_ports.keys())
+        # noinspection PyUnresolvedReferences
+        self.check_alive_subscriber.subscriber.setsockopt(zmq.SUBSCRIBE, ALIVE_TOPIC.encode())
+
+        def check_alive():
+            last_check_time = time.time()
+            while True:
+                received = self.check_alive_subscriber.recv(self.timeout)
+                if received is None:  # did not get any alive signal -> disconnect from all ips
+                    break
+                alive_ip, alive_port = received[1].split(":")
+                alive_port = int(alive_port)
+                current_time = time.time()
+                self.ip_and_ports[(alive_ip, alive_port)] = current_time  # update last alive time
+                if current_time - last_check_time > self.timeout / 1000:  # check all ips for alive
+                    last_check_time = current_time
+                    remaining_ips = {}
+                    for (ip, port), last_alive_time in self.ip_and_ports.items():
+                        if current_time - last_alive_time < timeout:
+                            remaining_ips[(ip, port)] = self.ip_and_ports[(ip, port)]
+                        else:
+                            self.subscriber.disconnect("tcp://{}:{}".format(ip, port))
+                    self.ip_and_ports = remaining_ips
+            self.ip_and_ports.clear()
+            self.subscriber.close()
+
+        self.timeout = timeout
+        if self.check_alive_thread is None:
+            self.check_alive_thread = Thread(target=check_alive, name="check_alive", args=())
+            self.check_alive_thread.setDaemon(True)
 
     def start(self):
         """
         Connects the subscriber to the given ip and port and subscribes to the given subjects
         Returns: (Subscriber) self
         """
-        self.subscriber.connect("tcp://{}:{}".format(self.ip, self.sub_port))
+        for ip, port in self.ip_and_ports:
+            self.subscriber.connect("tcp://{}:{}".format(ip, port))
         for s in self.subjects:
-            # noinspection PyUnresolvedReferences
-            # self.subscriber.set(zmq.SUBSCRIBE, s.encode())
+            if s == ALIVE_TOPIC:
+                raise ValueError("topic {} is reserved".format(ALIVE_TOPIC))
             # noinspection PyUnresolvedReferences
             self.subscriber.setsockopt(zmq.SUBSCRIBE, s.encode())
+        if self.check_alive_thread is not None:
+            self.check_alive_subscriber.start()
+            self.check_alive_thread.start()
         return self
 
     def recv(self, timeout=None):
@@ -51,6 +112,8 @@ class Subscriber:
                 else:
                     topic, payload = self.subscriber.recv_multipart()
                 message = msgpack.loads(payload)
+                if type(message) is bytes:
+                    message = message.decode()
                 topic = topic.decode()
                 return topic, message
             except zmq.error.Again:
@@ -76,6 +139,25 @@ class Publisher:
         self.publisher.bind("tcp://*:%s" % self.pub_port)
         return self
 
+    def send_alive_signal(self, interval):
+        """
+        Start a thread which sends a ALIVE_TOPIC message every interval milliseconds
+        with the publishers ips and port
+        Args:
+            interval(int): the interval in milliseconds
+        """
+        pub_ips = get_own_ips()
+
+        def send_alive():
+            while True:
+                for ip in pub_ips:
+                    self.send(ALIVE_TOPIC, "{}:{}".format(ip, self.pub_port))
+                time.sleep(interval / 1000)
+
+        send_alive_thread = Thread(target=send_alive, name="send_alive", args=())
+        send_alive_thread.setDaemon(True)
+        send_alive_thread.start()
+
     def send(self, topic, message):
         """
         Sends a message for a specific topic
@@ -86,3 +168,17 @@ class Publisher:
         print("Pub sends {} {}".format(topic, message))
         payload = msgpack.dumps(message)
         self.publisher.send_multipart((topic.encode(), payload))
+
+
+def get_own_ips():
+    """
+    Returns the ips of this device for every interface
+    Returns: (list(str)) the list of ips
+    """
+    own_ips = []
+    for interface in interfaces():
+        address_key = 'addr'
+        addresses = [i[address_key] for i in ifaddresses(interface).setdefault(AF_INET, [{'addr': None}]) if
+                     i[address_key] is not None]
+        own_ips.extend(addresses)
+    return own_ips
